f0ff7aaf715a5f644f6925276f9b982a3b115a4a
Stamos Prekas <prekasstamos@gmail.com>
2019-03-08 11:33:07 -0500

    Update cl_tablecam.lua

--- gamemode/cl_tablecam.lua
+++ gamemode/cl_tablecam.lua
@@ -17,8 +17,6 @@ end
 
 function myCalcView(Player, Origin, Angles, FieldOfView)--LookAtPoint is where-ever you want to look at. 1000 is how far away you want the camera to be
 	if !Player:GetNWBool("TableView", false) then
-		//print(LocalPlayer():GetNWBool("TableView"))
-		//print("normal view time")
 		local view = {}
 		view.origin = Origin
 		view.angles = Angles
@@ -26,35 +24,16 @@ function myCalcView(Player, Origin, Angles, FieldOfView)--LookAtPoint is where-e
 		tableView = false
 		return view
 	else
-		//print(LocalPlayer():GetNWBool("TableView"))
-		//print("calcview time")
 		local View = {};
 		View.origin = OrbitCamera(Player:GetPos() + Vector(0,0,70), (Angles:Forward()*-1):Angle(),100);
-		//print(Player:GetPos())
 		View.angles = Angles;
 		View.fov = FieldOfView;
 		View.drawviewer = true
-		//Player:SetVelocity(Vector(0,0,0))
 		tableView = true
 		return View;
 	end
 end
 
-
-/*hook.Add("Think","ThinkView",function()
-
-	//tableView = LocalPlayer():GetNWBool("TableView", false)
-	//tableView = LocalPlayer():GetNWBool("TableView", false)
-	if tableView then
-		hook.Add("CalcView", "CameraTableView", myCalcView)
-	else
-		hook.Add("CalcView", "CameraNormalView", NormalView)
-	end
-	//if tableView then
-		//LocalPlayer():SetLocalVelocity(Vector(0,0,0))
-	//end
-	//print(tableView)
-end)*/
 hook.Add("CalcView", "CameraView", myCalcView)
 
 // problem here: pressing E makes you sometimes leave the table right when you enter it. the delay needs to be reset upon entering a table
@@ -62,18 +41,11 @@ hook.Add("KEY_USE", "ExitTable", function()
 	if !LocalPlayer():GetNWBool("TableView") then print("tableview false") return end
 	if LocalPlayer():GetNWBool("PlayingTable", false) then print("playingtable true") return end
 	local timeElapsed = CurTime() - lastOccurrence
-	//print(timeElapsed .. " time elapsed")
-	//print(lastOccurrence .. " last occurrence")
 	if timeElapsed < delay then
 		print("table leave on cooldown")
 		print(timeElapsed .. " < " .. delay)
 	elseif (timeElapsed >= delay) then
-		//print("time elapsed is greater than delay")
 		lastOccurrence = CurTime()
-		//print(timeElapsed .. " > " .. delay)
-		//print(tableView)	
-		//print(tostring(tableView) .. " table view")
-		//LocalPlayer():SetNWBool("TableView", false)
 		net.Start("UpdateTableView")
 			net.WriteBool(false)
 		net.SendToServer()
@@ -84,7 +56,3 @@ hook.Add("KEY_USE", "ExitTable", function()
 		// in the future, i should offload the key_use thing to another script or something
 	end
 end)
-
-/*hook.Add("PlayerEnteredTable", "PlayerEnteredTable", function()
-	lastOccurence = CurTime()
-end)*/
