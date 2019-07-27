3a07d22201a543227b28484f6af30ce4bea5f666
Stamos Prekas <prekasstamos@gmail.com>
2019-07-26 23:14:34 -0400

    more zawas

--- gamemode/sounds.lua
+++ gamemode/sounds.lua
@@ -6,6 +6,15 @@ sound.Add({
 	pitch = {95, 105},
 	sound = {
 		"ambient/zawa1.wav",
-		"ambient/zawa2.wav"
+		"ambient/zawa2.wav",
+		"ambient/zawa3.wav",
+		"ambient/zawa4.wav",
+		"ambient/zawa5.wav",
+		"ambient/zawa6.wav",
+		"ambient/zawa7.wav",
+		"ambient/zawa8.wav",
+		"ambient/zawa9.wav",
+		"ambient/zawa10.wav",
+		"ambient/zawa11.wav"
 	}
 })
